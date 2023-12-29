aed98833607b25bd2a55076b422baaaac3b0ab50
Darshan Satone <darshansatone1996@gmail.com>
2023-12-29 09:00:22 +0530

    Done with trailing MA

--- TimeSeries(Centered_Trailing_MA).py
+++ TimeSeries(Centered_Trailing_MA).py
@@ -8,7 +8,9 @@ Created on Fri Dec 29 08:25:32 2023
 
 import pandas as pd
 import numpy as np
+from numpy import sqrt
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 import os
 os.chdir("/Users/darshmac/Documents/cdac/For DBDA/Advance Analytics/Datasets")
 
@@ -27,6 +29,16 @@ fcast = y_train.rolling(3,center =False).mean()
 MA = fcast.iloc[-1]
 MA_series = pd.Series(MA.repeat(len(y_test)))
 MA_fcast = pd.concat([fcast,MA_series],ignore_index=True)
+
+plt.plot(y_train,label='Train')
+plt.plot(y_test,label='Test')
+plt.plot(MA_fcast,label = 'Moving Average Forecast')
+plt.legend(loc = 'best')
+plt.show()
+
+rms = sqrt(mean_squared_error(y_test, MA_series))
+print(rms)
+
 
 
 
